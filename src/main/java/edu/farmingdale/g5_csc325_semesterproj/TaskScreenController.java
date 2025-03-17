9af2d836823ccee24ebd5462d1d3c687f2311bf4
Bhercules1023 <hercb@farmingdale.edu>
2025-03-17 14:26:58 -0400

    Update TaskScreenController.java
    Made slight changes to the button names. Added buttons to change to add, edit, and delete buttons (This may change depending on how the coding goes along the way). Added lines of code to add data to the columns.

--- src/main/java/edu/farmingdale/g5_csc325_semesterproj/TaskScreenController.java
+++ src/main/java/edu/farmingdale/g5_csc325_semesterproj/TaskScreenController.java
@@ -1,28 +1,67 @@
 package edu.farmingdale.g5_csc325_semesterproj;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TaskScreenController {
 
+
+    @FXML
+    public TableView<Task> taskTable;
+
+    @FXML
+    private TableColumn<Task, String> taskNameColumn;
+    @FXML
+    private TableColumn<Task, String> dateAssignedColumn;
+    @FXML
+    private TableColumn<Task, String> dueDateColumn;
+
     @FXML
     private Label usernameLabel;
 
     @FXML
-    private Button addTskBtn;
+    private Button addTask;
+    @FXML
+    private Button deleteTask;
+    @FXML
+    private Button editTask;
 
     @FXML
     private void initialize() {
-        usernameLabel.setText("User: " + MindMapApp.getUsername());
+        usernameLabel.setText(MindMapApp.getUsername());
+
+        taskNameColumn.setCellValueFactory(new PropertyValueFactory<>("taskName"));
+        dateAssignedColumn.setCellValueFactory(new PropertyValueFactory<>("dateAssigned"));
+        dueDateColumn.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+
+        taskTable.setItems(AddTaskScreenController.getTask());
+
     }
 
     @FXML
     private void handleAddTask() throws IOException {
-        Stage stage = (Stage) addTskBtn.getScene().getWindow();
+        Stage stage = (Stage) addTask.getScene().getWindow();
         MindMapApp.switchScene(stage, "add-task-screen.fxml");
     }
+
+    @FXML
+    private void handleEditTask() throws IOException {
+        Stage stage = (Stage) editTask.getScene().getWindow();
+        MindMapApp.switchScene(stage, "edit-task-screen.fxml");
+    }
+
+    @FXML
+    private void handleDeleteTask() throws IOException {
+        Stage stage = (Stage) deleteTask.getScene().getWindow();
+        MindMapApp.switchScene(stage, "delete-task-screen.fxml");
+    }
+
 }
